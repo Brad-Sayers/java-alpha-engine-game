4733ac420a0484723e946a8aafd0cc2bac7f8f1e
Brad Sayers <brad.sayers.ca@gmail.com>
2017-09-28 14:51:28 -0700

    9/28/2017 14:51

--- Dorifuto/src/Level1.java
+++ Dorifuto/src/Level1.java
@@ -6,6 +6,7 @@ public class Level1 extends GameLevel
 {
 	@Override public void create()
 	{
+		GameObject lot = new GameObject("Parkinglot",1920,1085,"Parkinglot.png");
 		GameObject hud = new GameObject("Hud",1920,1080,"Hud.png");
 		GameObject car = new Player();
 		ObjectManager.addGameObject(car);
@@ -18,32 +19,7 @@ public class Level1 extends GameLevel
 
 	@Override public void update(float v)
 	{
-		/*if (InputManager.isPressed(KeyEvent.VK_SPACE))
-		{
-			GameObject ClutchLight = new GameObject("ClutchLight",1920,1080,"ClutchLight.png");
-		}
-		if (InputManager.isReleased(KeyEvent.VK_SPACE))
-		{
-			ObjectManager.removeAllObjectsByName("ClutchLight");
-		}
-		if (InputManager.isPressed(KeyEvent.VK_W))
-		{
-			GameObject ThrottleLight = new GameObject("ThrottleLight",1920,1080,"ThrottleLight.png");
-		}
-		if (InputManager.isReleased(KeyEvent.VK_W))
-		{
-			ObjectManager.removeAllObjectsByName("ThrottleLight");
-		}
-		if (InputManager.isPressed(KeyEvent.VK_S))
-		{
-			GameObject BrakeLight = new GameObject("BrakeLight",1920,1080,"BrakeLight.png");
-		}
-		if (InputManager.isReleased(KeyEvent.VK_S))
-		{
-			ObjectManager.removeAllObjectsByName("BrakeLight");
-		}
-		System.out.println();
-		*/
+
 	}
 
 	@Override public void uninitialize()
--- Dorifuto/src/Player.java
+++ Dorifuto/src/Player.java
@@ -8,12 +8,12 @@ public class Player extends GameObject
 {
 	public Player()
 	{
-		super("ae86", 400, 220, "ae86.png");
-		setRotation(90);
+		super("ae86", 200, 110, "ae86.png");
+		setPositionX(-828);
 	}
 
 	public Vec2 direction = new Vec2(1.0f,0.0f);
-
+	int pauseCount = 0;
 	@Override public void update(float v)
 	{
 		float movementSpeed = 3.0f;
@@ -26,9 +26,9 @@ public class Player extends GameObject
 			float rotation = getRotation();
 			rotation -= rotationSpeed;
 			setRotation(rotation);
-			rotation = (float)Math.toRadians(rotation);
-			direction.setX((float)Math.cos(rotation));
-			direction.setY((float)Math.sin(rotation));
+			rotation = (float) Math.toRadians(rotation);
+			direction.setX((float) Math.cos(rotation));
+			direction.setY((float) Math.sin(rotation));
 		}
 		//Wheels Cut Left, Forward
 		if (InputManager.isPressed(KeyEvent.VK_A) && InputManager.isPressed(KeyEvent.VK_W))
@@ -36,9 +36,9 @@ public class Player extends GameObject
 			float rotation = getRotation();
 			rotation += rotationSpeed;
 			setRotation(rotation);
-			rotation = (float)Math.toRadians(rotation);
-			direction.setX((float)Math.cos(rotation));
-			direction.setY((float)Math.sin(rotation));
+			rotation = (float) Math.toRadians(rotation);
+			direction.setX((float) Math.cos(rotation));
+			direction.setY((float) Math.sin(rotation));
 		}
 		//Wheels Cut Right, Reverse
 		if (InputManager.isPressed(KeyEvent.VK_D) && InputManager.isPressed(KeyEvent.VK_S))
@@ -46,9 +46,9 @@ public class Player extends GameObject
 			float rotation = getRotation();
 			rotation += rotationSpeed;
 			setRotation(rotation);
-			rotation = (float)Math.toRadians(rotation);
-			direction.setX((float)Math.cos(rotation));
-			direction.setY((float)Math.sin(rotation));
+			rotation = (float) Math.toRadians(rotation);
+			direction.setX((float) Math.cos(rotation));
+			direction.setY((float) Math.sin(rotation));
 		}
 		//Wheels Cut Right, Forward
 		if (InputManager.isPressed(KeyEvent.VK_D) && InputManager.isPressed(KeyEvent.VK_W))
@@ -56,40 +56,30 @@ public class Player extends GameObject
 			float rotation = getRotation();
 			rotation -= rotationSpeed;
 			setRotation(rotation);
-			rotation = (float)Math.toRadians(rotation);
-			direction.setX((float)Math.cos(rotation));
-			direction.setY((float)Math.sin(rotation));
+			rotation = (float) Math.toRadians(rotation);
+			direction.setX((float) Math.cos(rotation));
+			direction.setY((float) Math.sin(rotation));
 		}
 		//Forward
 		if (InputManager.isPressed(KeyEvent.VK_W))
 		{
 			Vec2 position = getPosition();
-			position.add(Vec2.scale(direction,movementSpeed));
+			position.add(Vec2.scale(direction, movementSpeed));
 			setPosition(position);
 		}
 		//Reverse
 		if (InputManager.isPressed(KeyEvent.VK_S))
 		{
 			Vec2 position = getPosition();
-			position.subtract(Vec2.scale(direction,movementSpeed));
+			position.subtract(Vec2.scale(direction, movementSpeed));
 			setPosition(position);
 		}
-		//Dont go out of frame
-		if(getPositionX() >= 540)
-		{
-			setPositionX(539.9f);
-		}
-		if(getPositionX() <= -865)
-		{
-			setPositionX(-864.9f);
-		}
-		if(getPositionY() >= 355)
-		{
-			setPositionY(354.9f);
-		}
-		if(getPositionY() <= -409.0)
-		{
-			setPositionY(-408.9f);
-		}
+
+		//Side lights
+
+
+		//Pause menu
+
+
 	}
 }
